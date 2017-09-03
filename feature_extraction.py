a7e150ae099c7c79d1465b3f9168bc5d3ae517e7
Javier-AG <javiarredondog@gmail.com>
2017-09-03 21:12:31 +0200

    Low-level features extraction

---
+++ feature_extraction.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import numpy as np
+import subprocess
+
+# FEATURE EXTRACTION using Essentia's Out-of-box Freesound Extractor
+
+# Selection of samples. Change folder and run again!
+folder = 'FOLDER_PATH'
+pardir = 'PARENT_DIRECTORY_PATH' + folder + '/'
+filenames = os.listdir(pardir)
+samples = np.array([track for track in filenames if (track[-3:] == 'wav')])
+
+# Create yaml file per each sample. It can be slow for many samples...
+for track in samples:
+    subprocess.Popen(['./streaming_extractor_freesound ' + os.path.join(pardir,track) + ' ' + track[:-4]], shell=True)
+print "++++++++++++ EXTRACTION FINISHED ++++++++++++++"
